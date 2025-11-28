e440a809f64d1aed50ecff041c3017269d27aff9
Iyad <iyadshobaki82@gmail.com>
2025-11-28 16:51:08 -0500

    Feature: Add POST method to create a post and add the created post in the DOM

--- secret-diary/dist/script.dev.js
+++ secret-diary/dist/script.dev.js
@@ -8,19 +8,48 @@ function createPostMarkup(post) {
 
 function addPostToDOM(container, markup) {
   container.insertAdjacentHTML("afterbegin", markup);
-}
+} // function getPosts() {
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       data.forEach((post) => {
+//         const container = document.querySelector(".container");
+//         addPostToDOM(container, createPostMarkup(post));
+//       });
+//     })
+//     .catch((err) => {
+//       console.error("Error. The request failed: ", err);
+//     });
+// }
+// getPosts();
 
-function getPosts() {
-  fetch("https://jsonplaceholder.typicode.com/posts").then(function (res) {
+
+function createPost(newPost) {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: newPost.title,
+      body: newPost.body,
+      userId: 1
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  }).then(function (res) {
     return res.json();
-  }).then(function (data) {
-    data.forEach(function (post) {
-      var container = document.querySelector(".container");
-      addPostToDOM(container, createPostMarkup(post));
-    });
-  })["catch"](function (err) {
-    console.error("Error. The request failed: ", err);
+  }).then(function (post) {
+    addPostToDOM(document.querySelector(".container"), createPostMarkup(post));
   });
-}
+} // form submit event handler
 
-getPosts();
+
+document.forms.post.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var _event$currentTarget$ = event.currentTarget.elements,
+      title = _event$currentTarget$.title,
+      text = _event$currentTarget$.text;
+  createPost({
+    title: title.value,
+    body: text.value
+  });
+});
--- secret-diary/script.js
+++ secret-diary/script.js
@@ -13,19 +13,52 @@ function addPostToDOM(container, markup) {
   container.insertAdjacentHTML("afterbegin", markup);
 }
 
-function getPosts() {
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((post) => {
-        const container = document.querySelector(".container");
+// function getPosts() {
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       data.forEach((post) => {
+//         const container = document.querySelector(".container");
 
-        addPostToDOM(container, createPostMarkup(post));
-      });
-    })
-    .catch((err) => {
-      console.error("Error. The request failed: ", err);
+//         addPostToDOM(container, createPostMarkup(post));
+//       });
+//     })
+//     .catch((err) => {
+//       console.error("Error. The request failed: ", err);
+//     });
+// }
+
+// getPosts();
+
+function createPost(newPost) {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: newPost.title,
+      body: newPost.body,
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((res) => res.json())
+    .then((post) => {
+      addPostToDOM(
+        document.querySelector(".container"),
+        createPostMarkup(post)
+      );
     });
 }
 
-getPosts();
+// form submit event handler
+document.forms.post.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const { title, text } = event.currentTarget.elements;
+
+  createPost({
+    title: title.value,
+    body: text.value,
+  });
+});
