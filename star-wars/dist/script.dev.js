81c209261026ffbee6ddfc737486f170e2881b3d
Iyad <iyadshobaki82@gmail.com>
2025-11-28 20:06:16 -0500

    feature: Add request to star wars API to get different info and add it to DOM

--- star-wars/dist/script.dev.js
+++ star-wars/dist/script.dev.js
@@ -5,6 +5,47 @@ var content = document.querySelector(".content");
 var result = document.querySelector(".content__result");
 var error = document.querySelector(".content__error");
 var spinner = document.querySelector(".spinner");
+
+function search(entity, entityId) {
+  return fetch("https://swapi.nomoreparties.co/".concat(entity, "/").concat(entityId));
+}
+
+function renderResult(text) {
+  result.textContent = text;
+  error.textContent = "";
+}
+
+function renderError(err) {
+  error.textContent = err;
+  result.textContent = "";
+}
+
+function renderLoading(isLoading) {
+  if (isLoading) {
+    spinner.classList.add("spinner_visible");
+    content.classList.add("content_hidden");
+  } else {
+    spinner.classList.remove("spinner_visible");
+    content.classList.remove("content_hidden");
+  }
+}
+
 form.addEventListener("submit", function submit(e) {
   e.preventDefault();
+  renderLoading(true);
+  search(form.elements.entity.value, form.elements.entityId.value).then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+
+    return Promise.reject(res.status);
+  }).then(function (res) {
+    renderResult(res.name);
+    console.log(res);
+  })["catch"](function (err) {
+    renderError("Error: ".concat(err));
+    console.log("Error: ".concat(err));
+  })["finally"](function () {
+    renderLoading(false);
+  });
 });
--- star-wars/script.js
+++ star-wars/script.js
@@ -4,6 +4,46 @@ const result = document.querySelector(".content__result");
 const error = document.querySelector(".content__error");
 const spinner = document.querySelector(".spinner");
 
+function search(entity, entityId) {
+  return fetch(`https://swapi.nomoreparties.co/${entity}/${entityId}`);
+}
+function renderResult(text) {
+  result.textContent = text;
+  error.textContent = "";
+}
+function renderError(err) {
+  error.textContent = err;
+  result.textContent = "";
+}
+
+function renderLoading(isLoading) {
+  if (isLoading) {
+    spinner.classList.add("spinner_visible");
+    content.classList.add("content_hidden");
+  } else {
+    spinner.classList.remove("spinner_visible");
+    content.classList.remove("content_hidden");
+  }
+}
 form.addEventListener("submit", function submit(e) {
   e.preventDefault();
+  renderLoading(true);
+  search(form.elements.entity.value, form.elements.entityId.value)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(res.status);
+    })
+    .then((res) => {
+      renderResult(res.name);
+      console.log(res);
+    })
+    .catch((err) => {
+      renderError(`Error: ${err}`);
+      console.log(`Error: ${err}`);
+    })
+    .finally(() => {
+      renderLoading(false);
+    });
 });
